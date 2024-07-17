5ff2e65ceee816d08a3c221153b4ea4cf76811c5
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-07-17 23:31:34 +0900

    refactor: GlobalStyle 이용하여 root 전역적으로 적용되도록 수정

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,6 +1,14 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
+  #root {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+    height: 100vh;
+    width: 100%;
+  }
   body {
     position: relative;
     margin: 0 auto;
@@ -12,6 +20,7 @@ const GlobalStyle = createGlobalStyle`
     background-color: #F8F8F8;
     border-color: black;
     align-items: center;
+    padding-top: 24px; /* 상태바 높이만큼 상단 여백 추가 */
   }
 `
 
