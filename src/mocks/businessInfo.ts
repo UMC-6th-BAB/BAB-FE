29258f11451f8c5f7d99c7ec3533a82af62abf3c
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-07-19 20:17:23 +0900

    chore: mock 데이터 배열로 변경

--- src/mocks/businessInfo.ts
+++ src/mocks/businessInfo.ts
@@ -6,10 +6,12 @@ export interface BusinessInfo {
   item: string
 }
 
-export const businessInfo: BusinessInfo = {
-  registrationNumber: 1111,
-  address: '준영정',
-  businessAddress: '경기도 김포시 북변동',
-  industry: '업태',
-  item: '종목',
-}
+export const businessInfo: BusinessInfo[] = [
+  {
+    registrationNumber: 1111,
+    address: '준영정',
+    businessAddress: '경기도 김포시 북변동',
+    industry: '업태',
+    item: '종목',
+  },
+]
