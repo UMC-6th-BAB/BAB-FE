ffef49bc35d1a1744142a597741a8c88b4af1e79
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-07-21 14:53:59 +0900

    fix: 절대경로 적용 안되는 에러 해결

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,11 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      '@apis': path.resolve(__dirname, 'src/apis'),
       '@components': path.resolve(__dirname, 'src/components'),
       '@assets': path.resolve(__dirname, 'src/assets'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@mocks': path.resolve(__dirname, 'src/mocks'),
       '@pages': path.resolve(__dirname, 'src/pages'),
       '@styles': path.resolve(__dirname, 'src/styles'),
       '@icons': path.resolve(__dirname, 'src/assets/icons'),
