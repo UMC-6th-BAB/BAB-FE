43cd1b7637cc6ef7e1a5617ea9575c0542f0e384
sihyun2988 <sihyun2988@gmail.com>
2024-07-22 16:42:33 +0900

    membertype localstorage

--- src/pages/MemberType/MemberType.tsx
+++ src/pages/MemberType/MemberType.tsx
@@ -5,12 +5,21 @@ import { MemberContainer, Welcome, SelectMessage, MemberButton, StartButton } fr
 export const MemberType: React.FC = () => {
     const navigate = useNavigate();
 
+    const handleInputStudent=()=>{
+        localStorage.setItem('membertype', 'student');
+        console.log('student local storage');
+    }
+    const handleInputManager=()=>{
+        localStorage.setItem('membertype', 'manager');
+        console.log('manager local storage')
+    }
+
     return(
         <MemberContainer>
             <Welcome>이시현님, <br/>환영합니다!</Welcome>
             <SelectMessage>서비스 이용을 위해<br/>회원 유형을 선택해주세요.</SelectMessage>
-            <MemberButton>학생이에요</MemberButton>
-            <MemberButton>사장님이에요</MemberButton>
+            <MemberButton onClick={handleInputStudent}>학생이에요</MemberButton>
+            <MemberButton onClick={handleInputManager}>사장님이에요</MemberButton>
             <StartButton onClick={()=>navigate("/signup")}>밥이득 시작하기</StartButton>
         </MemberContainer>
     )
--- src/pages/OAuth/OAuth.tsx
+++ src/pages/OAuth/OAuth.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
-import {useNavigate} from 'react-router-dom'
-import { LogoContainer, Logo, Title, SignupButton } from './Signup.style';
-import {Kakao_Auth_Url} from '../../components/OAuthUrl'
+import axios from 'axios';
+import SyncLoader from "react-spinners/SyncLoader"
+import {LoadContainer} from './OAuth.style'
 
 export const OAuth: React.FC = () => {
+    //인가코드
+    let code = new URL(window.location.href).searchParams.get("code");
+
+    const options = {
+        url: 'https://kauth.kakao.com/oauth/token',//임시 서버 주소, 백엔드 api 명세서 나오면 그때 수정
+        method: 'POST',
+        data:{
+            code: code //인가코드 서버로 넘기기
+        }
+    };
+    
+    axios(options)
+        .then(response=>console.log(response))
+    .catch(error => {
+        console.error("Error during OAuth process:", error);
+    })
 
     return (
-        <div></div>
+        <LoadContainer>
+            <SyncLoader/>
+        </LoadContainer>
     );
 };
