092a4cbb9ed52a2ab3b6c023fac753f34545023e
solmee <kii5541@naver.com>
2024-07-23 23:18:12 +0900

    feat: 메뉴 정보 추가하는 RegisterMenu 컴포넌트 구현

---
+++ src/components/RegisterMenu/RegisterMenu.style.ts
@@ -0,0 +1,42 @@
+import styled from 'styled-components'
+
+export const StyledMenuRow = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+  width: 100%;
+`
+export const StyledMenuInputContainer = styled.div`
+  /* display: flex;
+  flex-direction: column;
+  width: calc(100% - 103px); */
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+`
+export const StyledUploadBox = styled.div`
+  box-sizing: border-box;
+  border: 0.4px solid #e7e7e7;
+  width: 93px;
+  height: 93px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #ffffff;
+  border-radius: 5px;
+  margin-right: 10px;
+`
+
+export const StyledUploadImg = styled.img`
+  height: 29px;
+  width: 29px;
+  /* padding-top: 5%; */
+`
+export const StyledMenuInput = styled.input`
+  width: 205px;
+  height: 36px;
+  padding-left: 10px;
+  margin-bottom: 10px;
+  border: 0.4px solid #e7e7e7;
+  border-radius: 5px;
+`
---
+++ src/components/RegisterMenu/RegisterMenu.tsx
@@ -0,0 +1,41 @@
+import Camera from '../../assets/RegisterStoreInfo/camera.png'
+import {
+  StyledMenuInput,
+  StyledMenuInputContainer,
+  StyledMenuRow,
+  StyledUploadBox,
+  StyledUploadImg,
+} from './RegisterMenu.style'
+
+interface RegisterMenuProps {
+  index: number
+  menu: {
+    name: string
+    price: string
+  }
+  onChange: (e: React.ChangeEvent<HTMLInputElement>, index: number) => void
+}
+
+export const RegisterMenu = ({ index, menu, onChange }: RegisterMenuProps) => (
+  <StyledMenuRow>
+    <StyledUploadBox>
+      <StyledUploadImg src={Camera} alt="업로드 아이콘" />
+    </StyledUploadBox>
+    <StyledMenuInputContainer>
+      <StyledMenuInput
+        type="text"
+        name="name"
+        placeholder="메뉴 이름"
+        value={menu.name}
+        onChange={(e) => onChange(e, index)}
+      />
+      <StyledMenuInput
+        type="text"
+        name="price"
+        placeholder="가격"
+        value={menu.price}
+        onChange={(e) => onChange(e, index)}
+      />
+    </StyledMenuInputContainer>
+  </StyledMenuRow>
+)
