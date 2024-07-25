d99a6e29fbfe94c41b72fd1924508fed74ff3b52
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-07-25 17:20:35 +0900

    design: 라우팅에 따른 Footer 컴포넌트 랜더링 분리 및 퍼블리싱

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import styled, { ThemeProvider } from 'styled-components'
 import GlobalStyle from '@styles/GlobalStyle'
 import { theme } from '@styles/theme'
-import ManagerPage from '@pages/ManagerPage/ManagerPage'
-import Footer from '@components/Footer/Footer'
-import StatusBar from '@components/StatusBar/StatusBar'
-import DiscountEventPage from '@pages/DiscountEventPage/DiscountEventPage'
-
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  width: 100%;
-  height: 100%;
-`
+import AppContent from '@components/AppContent'
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Router>
-        <StatusBar />
-        <Routes>
-          <Route path="/" element={<Container>임시 메인 페이지</Container>} />
-          <Route path="/manager" element={<ManagerPage />} />
-          <Route path="/discount-event" element={<DiscountEventPage />} />
-        </Routes>
-        <Footer />
+        <AppContent />
       </Router>
     </ThemeProvider>
   )
---
+++ src/components/AppContent.tsx
@@ -0,0 +1,38 @@
+import { Routes, Route, useLocation } from 'react-router-dom'
+import styled, { ThemeProvider } from 'styled-components'
+import ManagerPage from '@pages/ManagerPage/ManagerPage'
+import Footer from '@components/Footer/Footer'
+import StatusBar from '@components/StatusBar/StatusBar'
+import DiscountEventPage from '@pages/DiscountEventPage/DiscountEventPage/DiscountEventPage'
+import DiscountEventPageTwo from '@pages/DiscountEventPage/DiscountEventPageTwo/DiscountEventPageTwo'
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+`
+
+export default function AppContent() {
+  const location = useLocation()
+  const showFooter =
+    location.pathname === '/manager' ||
+    location.pathname === '/student-page' ||
+    location.pathname === '/' ||
+    location.pathname === '/list'
+
+  return (
+    <>
+      <StatusBar />
+      <Routes>
+        <Route path="/" element={<Container>임시 메인 페이지</Container>} />
+        <Route path="/manager" element={<ManagerPage />} />
+        <Route path="/discount-event" element={<DiscountEventPage />} />
+        <Route path="/discount-eventTwo" element={<DiscountEventPageTwo />} />
+      </Routes>
+      {showFooter && <Footer />}
+    </>
+  )
+}
