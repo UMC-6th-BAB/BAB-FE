d8c37db088ff9b946739ee6e72c2dd85345207b9
solmee <kii5541@naver.com>
2024-07-30 22:48:46 +0900

    feat: loading 컴포넌트 구현

---
+++ src/components/Loading/loading.style.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const StyledLoadingContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`
+
+export const StyledSpinner = styled.img`
+  width: 85px;
+  height: 85px;
+`
+
+export const StyledText = styled.p`
+  font-weight: 600px;
+  font-size: 22px;
+  color: #000000;
+`
---
+++ src/components/Loading/loading.tsx
@@ -0,0 +1,15 @@
+import Spinner from '@assets/BusinessUploadPage/loading.svg'
+import {
+  StyledLoadingContainer,
+  StyledSpinner,
+  StyledText,
+} from './loading.style'
+
+export default function Loading() {
+  return (
+    <StyledLoadingContainer>
+      <StyledSpinner src={Spinner} />
+      <StyledText>정보를 인식하고 있어요.</StyledText>
+    </StyledLoadingContainer>
+  )
+}
