eda6fcd8f8285c4d5bc774c9391cf525b06b17ec
yongmin01 <heart77123@naver.com>
2024-08-09 17:34:18 +0900

    design: StatusBar 주석처리

--- src/components/AppContent.tsx
+++ src/components/AppContent.tsx
@@ -31,7 +31,7 @@ export default function AppContent() {
     location.pathname === '/map'
   return (
     <>
-      <StatusBar />
+      {/* <StatusBar /> */}
       <Routes>
         <Route path="/" element={<Splash />} />
         <Route path="/memberType" element={<MemberType />} />
